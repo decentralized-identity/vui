c0fa55d22888dcef8d98b9404ff48b6e74077791
Markus Sabadello <markus@danubetech.com>
2020-11-18 19:11:41 +0100

    Add missing references DID-RESOLUTION and LD-PROOFS.

---
+++ common.js
@@ -0,0 +1,29 @@
+/* globals omitTerms, respecConfig, $, require */
+/* exported linkCrossReferences, restrictReferences, fixIncludes */
+
+var ccg = {
+  // Add as the respecConfig localBiblio variable
+  // Extend or override global respec references
+  localBiblio: {
+    "DID-RESOLUTION": {
+      title: "Decentralized Identifier Resolution",
+      href: "https://w3c-ccg.github.io/did-resolution/",
+      authors: [
+        "Markus Sabadello",
+        "Dmitri Zagidulin"
+      ],
+      status: "Draft Community Group Report",
+      publisher: "Credentials Community Group"
+    },
+    "LD-PROOFS": {
+      title: "Linked Data Proofs",
+      href: "https://w3c-dvcg.github.io/ld-proofs/",
+      authors: [
+        "Manu Sporny",
+        "Dave Longley"
+      ],
+      status: "CG-DRAFT",
+      publisher: "Digital Verification Community Group"
+    }
+  }
+};
