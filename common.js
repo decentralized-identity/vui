beade2308b9c6c76dba924375399c0a596a9c929
José San Juan <jose@gataca.io>
2022-01-12 12:30:24 +0100

    Base structure

--- common.js
+++ common.js
@@ -8,22 +8,16 @@ var ccg = {
     "DID-RESOLUTION": {
       title: "Decentralized Identifier Resolution",
       href: "https://w3c-ccg.github.io/did-resolution/",
-      authors: [
-        "Markus Sabadello",
-        "Dmitri Zagidulin"
-      ],
+      authors: ["Markus Sabadello", "Dmitri Zagidulin"],
       status: "Draft Community Group Report",
-      publisher: "Credentials Community Group"
+      publisher: "Credentials Community Group",
     },
     "LD-PROOFS": {
       title: "Linked Data Proofs",
       href: "https://w3c-dvcg.github.io/ld-proofs/",
-      authors: [
-        "Manu Sporny",
-        "Dave Longley"
-      ],
+      authors: ["Manu Sporny", "Dave Longley"],
       status: "CG-DRAFT",
-      publisher: "Digital Verification Community Group"
-    }
-  }
+      publisher: "Digital Verification Community Group",
+    },
+  },
 };
